d8a6fa65afb83a794b038fe477708afe5a36b699
Sarun6618 <ch.sarun0904@gmail.com>
2024-03-07 23:49:20 +0530

    added mongodb access

--- src/configs/mongodb.config.ts
+++ src/configs/mongodb.config.ts
@@ -1,13 +1,19 @@
 import { MongoClient } from "mongodb";
+
 export const mongoConfig = {
   expiresAt: process.env.MONGODB_EXPIRY_TIME,
   url: process.env.MONGODB_URL || "",
   dbName: process.env.MONGODB_DB_NAME || "",
   collectionName: "tokens",
 };
+const uri =
+  `${mongoConfig.url}/${mongoConfig.dbName}` +
+  "?retryWrites=true&w=majority" +
+  "&tls=true" +
+  `&tlsCAFile=${encodeURIComponent("../../X509-cert-2617992580617193489.pem")}`;
 
 export const mongoClient = new MongoClient(
-  mongoConfig.url
+  `${mongoConfig.url}/${mongoConfig.dbName} `
   //   {
   //   useNewUrlParser: true, // Make sure to include the options if needed
   //   useUnifiedTopology: true,
